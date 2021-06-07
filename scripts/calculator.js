d33ff11724e904e3fef7645f67907dcd4bc1e167
dudumoraes <85453161+dudumoraes@users.noreply.github.com>
2021-06-07 23:53:42 +0300

    Update calculator.js

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,7 +1,7 @@
+
 let num1;
 let currentNum;
 let operation;
-
 const display = document.querySelector('input#numDisplay');
 
 function numButton(num) {
@@ -10,17 +10,25 @@ function numButton(num) {
 }
 
 function add() {
-    operation = '+';
-    num1 = Number(display.value);
-    display.value += '+';
-    currentNum = '';
+    operation = '+'
+    num1 = Number(display.value)
+    display.value += '+'
+    currentNum = ''
+}
+function subtract() {
+    operation = '-'
+    num1 = Number(display.value)
+    display.value += '-'
+    currentNum = ''
 }
 
 function resolve() {
+    console.log(operation)
+    let result;
     if (operation == '+') {
-        const result = num1 + Number(currentNum);
+     result = num1 + Number(currentNum)
     } else if (operation == '-') {
-
+     result = num1 - Number(currentNum)
     } else if (operation == '*') {
 
     } else {
