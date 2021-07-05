89bd7702881ab6f1f16527d805f93714264d5246
dudumoraes <85453161+dudumoraes@users.noreply.github.com>
2021-07-05 15:05:30 -0300

    Create clock.js

---
+++ scripts/clock.js
@@ -0,0 +1,24 @@
+function load() {
+    let helloDiv = window.document.getElementById('hello')
+    let imagem = window.document.getElementById('img')
+    let now = new Date()
+    let hours = '22'//now.getHours() 
+    let minutes = now.getMinutes()
+
+    if (hours >= 0 && hours < 12) {
+        let text = 'Bom dia'
+        helloDiv.innerHTML = `${text}! Agora são ${hours}h${minutes}min`
+        imagem.src = 'ibagens/manha.png'
+        document.body.style.background = '#fdeb81'
+    } else if (hours >= 12 && hours <= 18) {
+        let text = 'Boa tarde'
+        helloDiv.innerHTML = `${text}! Agora são ${hours}h${minutes}min`
+        imagem.src = 'ibagens/tarde.png'
+        document.body.style.background = '#fe7802'
+    } else {
+        let text = 'Boa noite'
+        helloDiv.innerHTML = `${text}! Agora são ${hours}h${minutes}min`
+        imagem.src = 'ibagens/noite.png'
+        document.body.style.background  = '#23373a'
+    }
+}    
