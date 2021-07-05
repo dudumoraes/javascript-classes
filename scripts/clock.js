ff25acb9745797f50574ff3e0cbfcedf034f2ec8
dudumoraes <85453161+dudumoraes@users.noreply.github.com>
2021-07-05 16:02:14 -0300

    Update clock.js

--- scripts/clock.js
+++ scripts/clock.js
@@ -8,17 +8,17 @@ function load() {
     if (hours >= 0 && hours < 12) {
         let text = 'Bom dia'
         helloDiv.innerHTML = `${text}! Agora são ${hours}h${minutes}min`
-        imagem.src = 'https://github.com/dudumoraes/javascript-classes/issues/1#issue-937299469'
+        imagem.src = 'images/manha.png'
         document.body.style.background = '#fdeb81'
     } else if (hours >= 12 && hours <= 18) {
         let text = 'Boa tarde'
         helloDiv.innerHTML = `${text}! Agora são ${hours}h${minutes}min`
-        imagem.src = 'https://github.com/dudumoraes/javascript-classes/issues/2#issue-937301057'
+        imagem.src = 'images/tarde.png'
         document.body.style.background = '#fe7802'
     } else {
         let text = 'Boa noite'
         helloDiv.innerHTML = `${text}! Agora são ${hours}h${minutes}min`
-        imagem.src = 'https://github.com/dudumoraes/javascript-classes/issues/3#issue-937301238'
+        imagem.src = 'images/noite.png'
         document.body.style.background  = '#23373a'
     }
 }    
